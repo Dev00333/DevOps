659653d89b412f36bd22dceeacbc2597f0816c4c
Devang <devangmshr@gmail.com>
2026-06-11 15:15:30 +0530

    s3 bucket destruction problem fixed

--- jenkins_ec2_setup/step_1/s3.tf
+++ jenkins_ec2_setup/step_1/s3.tf
@@ -1,5 +1,6 @@
 resource aws_s3_bucket remote-s3-devang {
     bucket = "remote-backend-backup"
+    force_destroy = true
     tags = {
         Name = "remote-backend-backup"
         environment = "Dev"
